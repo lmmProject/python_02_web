614e25cf218b325ef05db9bd0ef0ea1b053d7c5e
lmm <752634866@qq.com>
2018-11-16 10:13:41 +0800

    web

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+'''
+Default configurations.
+'''
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'lmmlmm',
+        'db': 'demo_02_web'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/handlers.py
@@ -0,0 +1,12 @@
+' url handlers '
+from www.coroweb import get
+from www.models import User
+
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
