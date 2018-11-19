414d127614f24ba0d0246e76e7d1e5ff9086ccde
lmm <752634866@qq.com>
2018-11-19 17:02:58 +0800

    register/signin

--- www/handlers.py
+++ www/handlers.py
@@ -1,7 +1,11 @@
 ' url handlers '
 import re, time, json, logging, hashlib, base64, asyncio
-from www.coroweb import get
+
+from www.apis import APIValueError, APIError
+from www.coroweb import get, post
 from www.models import User, Comment, Blog, next_id
+from aiohttp import web
+from www.config import configs
 
 
 @get('/')
@@ -16,7 +20,158 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+
 # Web框架的设计是完全从使用者出发，目的是让使用者编写尽可能少的代码。
 #
 # 编写简单的函数而非引入request和web.Response还有一个额外的好处，
 # 就是可以单独测试，否则，需要模拟一个request才能测试。
+
+COOKIE_NAME = 'lmmsession'
+_COOKIE_KEY = configs.session.secret
+
+
+def user2cookie(user, max_age):
+    '''
+    Generate cookie str by user.
+    '''
+    # build cookie string by: id-expires-sha1
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+@post('/api/users')
+def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    shal_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(shal_passwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+    # make session cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+# 用户登录比用户注册复杂。由于HTTP协议是一种无状态协议，而服务器要跟踪用户状态，就只能通过cookie实现。
+# 大多数Web框架提供了Session功能来封装保存用户状态的cookie。
+#
+# Session的优点是简单易用，可以直接从Session中取出用户登录信息。
+# Session的缺点是服务器需要在内存中维护一个映射表来存储用户登录信息，
+# 如果有两台以上服务器，就需要对Session做集群，因此，使用Session的Web App很难扩展。
+#
+# 我们采用直接读取cookie的方式来验证用户登录，每次用户访问任意URL，都会对cookie进行验证，
+# 这种方式的好处是保证服务器处理任意的URL都是无状态的，可以扩展到多台服务器。
+#
+# 由于登录成功后是由服务器生成一个cookie发送给浏览器，所以，要保证这个cookie不会被客户端伪造出来。
+# 实现防伪造cookie的关键是通过一个单向算法（例如SHA1），举例如下：
+#
+# 当用户输入了正确的口令登录成功后，服务器可以从数据库取到用户的id，并按照如下方式计算出一个字符串：
+#
+# "用户id" + "过期时间" + SHA1("用户id" + "用户口令" + "过期时间" + "SecretKey")
+
+# 当浏览器发送cookie到服务器端后，服务器可以拿到的信息包括：
+# 用户id
+# 过期时间
+# SHA1值
+# 如果未到过期时间，服务器就根据用户id查找用户口令，并计算：
+#
+# SHA1("用户id" + "用户口令" + "过期时间" + "SecretKey")
+# 并与浏览器cookie中的哈希进行比较，如果相等，则说明用户已登录，否则，cookie就是伪造的。
+#
+# 这个算法的关键在于SHA1是一种单向算法，即可以通过原始字符串计算出SHA1结果，
+# 但无法通过SHA1结果反推出原始字符串。
+#
+# 所以登录API可以实现如下：
+
+
+@post('/api/authenticate')
+def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password.')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    # check passwd:
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    # authenticate ok, set cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
