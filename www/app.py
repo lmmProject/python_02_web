d385416288270fa38e39876429d3945563aecf27
lmm <752634866@qq.com>
2018-11-19 17:08:38 +0800

    format

--- www/app.py
+++ www/app.py
@@ -160,6 +160,7 @@ def init(loop):
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
